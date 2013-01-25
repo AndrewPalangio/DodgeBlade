43460ce17b24ed9a30c555ea06cf15650fc0d9ae
GeordieP <geordie.powers@gmail.com>
2013-01-24 23:15:21 -0500

    Assignment 1 Completed

--- DodgeBlade/DodgeBlade/Game1.cs
+++ DodgeBlade/DodgeBlade/Game1.cs
@@ -17,12 +17,19 @@ namespace DodgeBlade {
         SpriteBatch spriteBatch;
 
         Texture2D spike, wario, bg;
+        const byte NUM_WARIOS = 10, NUM_SPIKES = 5;
 
-        Sprite[] sprites = new Sprite[15];
-
+        Sprite[] sprites;
         Random random = new Random();
+        const String SPIKE = "Spike", WARIO = "Wario";
 
-        Color mycolor = new Color(255, 255, 255);
+        KeyboardState kbState;
+        MouseState mouseState;
+
+        // my vars
+        const byte IDLE = 0, LEFT = 1, UP = 2, RIGHT = 3, DOWN = 4;
+        bool isGamePaused = false;
+        byte wariosKilled = 0;
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -34,8 +41,21 @@ namespace DodgeBlade {
         }
 
         protected override void Initialize() {
-
             base.Initialize();
+        }
+
+        private void InitGame() {
+            sprites = new Sprite[NUM_SPIKES + NUM_WARIOS];
+
+            for (int i = 0; i < sprites.Length; i++) {
+                if (i < NUM_SPIKES) {
+                    sprites[i] = new Sprite(spike, new Vector2(random.Next((spike.Width / 2) + 1, (GraphicsDevice.Viewport.Width - spike.Width / 2 - 1)), random.Next((spike.Height / 2 + 1), (GraphicsDevice.Viewport.Height - spike.Height / 2 - 1))), new Vector2(random.Next(-200, 201), random.Next(-200, 201)), true, 3, 1, SpriteEffects.None);
+                    sprites[i].TextureImage.Name = SPIKE;
+                } else if (i < (NUM_WARIOS + NUM_SPIKES)) {
+                    sprites[i] = new Sprite(wario, new Vector2(random.Next((wario.Width / 2) + 1, (GraphicsDevice.Viewport.Width - wario.Width / 2 - 1)), random.Next((wario.Height / 2 + 1), (GraphicsDevice.Viewport.Height - wario.Height / 2 - 1))), new Vector2(random.Next(-200, 201), random.Next(-200, 201)), true, 0, 1, SpriteEffects.None);
+                    sprites[i].TextureImage.Name = WARIO;
+                }
+            }
         }
 
         protected override void LoadContent() {
@@ -45,23 +65,67 @@ namespace DodgeBlade {
             spike = Content.Load<Texture2D>("images/spike00");
             wario = Content.Load<Texture2D>("images/wario00");
 
-            for (int i = 0; i < sprites.Length; i++) {
-                if (i <= 5) {
-                    sprites[i] = new Sprite(spike, new Vector2(random.Next((spike.Width / 2) + 1, (GraphicsDevice.Viewport.Width - spike.Width / 2 - 1)), random.Next((spike.Height / 2 + 1), (GraphicsDevice.Viewport.Height - spike.Height / 2 - 1))), new Vector2(random.Next(100, 201), random.Next(100, 201)), true, 0, 1, SpriteEffects.None);
-                }
-                else {
-                    sprites[i] = new Sprite(wario, new Vector2(random.Next((wario.Width / 2) + 1, (GraphicsDevice.Viewport.Width - wario.Width/2 -1)), random.Next((wario.Height / 2 + 1), (GraphicsDevice.Viewport.Height - wario.Height / 2 - 1))), new Vector2(random.Next(100, 201), random.Next(100, 201)), true, 0, 1, SpriteEffects.None);
-                }
-            } 
+            InitGame();
         }
 
         protected override void UnloadContent() {
         }
 
-        protected override void Update(GameTime gameTime) {
+        public void Win() {
+            System.Windows.Forms.MessageBox.Show("You Win!");
+            Exit();
+        }
 
-            foreach (Sprite s in sprites) {
-                s.Update(gameTime, GraphicsDevice);
+        public void Lose() {
+            System.Windows.Forms.MessageBox.Show("You Lose!");
+            Exit();
+        }
+
+
+        protected override void Update(GameTime gameTime) {
+            kbState = Keyboard.GetState(PlayerIndex.One);
+
+            if (kbState.IsKeyDown(Keys.F2)){
+                InitGame();
+            } else if (kbState.IsKeyDown(Keys.Escape)){
+                if (isGamePaused) {
+                    isGamePaused = false;
+                } else {
+                    isGamePaused = true;
+                }
+            }
+
+            // Handle Mouse
+            mouseState = Mouse.GetState();
+
+           if (!isGamePaused) {
+               foreach (Sprite s in sprites) {
+                   if (mouseState.LeftButton == ButtonState.Pressed) {
+                       if (s.CollisionMouse(mouseState.X, mouseState.Y)) {
+                           if (s.TextureImage.Name == SPIKE) {
+                               Lose();
+
+                           } else if (s.TextureImage.Name == WARIO) {
+                               if (++wariosKilled >= 10) {
+                                   Win();
+                               }
+
+                               //for (int i = 4; i < sprites.Length; i++) {
+                               //    if (sprites[i].TextureImage.Name == WARIO && sprites[i].Alive == true) {
+                               //        break;
+                               //    } else {
+                               //        Console.WriteLine("{0}, name: {1}, alive: {2}", i, sprites[i].TextureImage.Name, sprites[i].Alive);
+                               //        Win();
+                               //    }
+                               //}
+                           }
+                       }
+                   }
+
+                   s.Update(gameTime, GraphicsDevice);
+
+               }
+
             }
 
             base.Update(gameTime);
@@ -71,13 +135,10 @@ namespace DodgeBlade {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-
             spriteBatch.Draw(bg, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
-
             foreach (Sprite s in sprites) {
                 s.Draw(gameTime, spriteBatch);
             }
-
             spriteBatch.End();
 
             base.Draw(gameTime);
--- SpriteClasses/Sprite.cs
+++ SpriteClasses/Sprite.cs
@@ -12,14 +12,14 @@ using Microsoft.Xna.Framework.Media;
 namespace SpriteClasses{
     public class Sprite{
 
-        public Vector2 currentVelocity, originalVelocity, position, spriteOrigin;
+        public Vector2 originalVelocity, position, spriteOrigin, velocity;
 
-        public Vector2 CurrentVelocity {
+        public Vector2 Velocity {
             get {
-                return currentVelocity;
+                return velocity;
             }
             set {
-                currentVelocity = value;
+                velocity = value;
             }
         }
 
@@ -50,13 +50,27 @@ namespace SpriteClasses{
             }
         }
 
-        public bool Active { get; set; }
+        public bool Alive { get; set; }
         public float Rotation { get; set; }
         public float RotationSpeed { get; set; }
         public float Scale { get; set; }
-        public bool setOrigin { get; set; }
         public SpriteEffects SpriteEffect { get; set; }
         public Texture2D TextureImage { get; set; }
+
+        public Rectangle CollisionRectangle {
+            get {
+                if (SpriteOrigin != Vector2.Zero) {
+                    return new Rectangle((int)Position.X, (int)Position.Y, (int)(TextureImage.Width * Scale), (int)(TextureImage.Height * Scale));
+                } else {
+                    return new Rectangle((int)SpriteOrigin.X, (int)SpriteOrigin.Y, (int)(TextureImage.Width * Scale), (int)(TextureImage.Height * Scale));
+                }
+            }
+        }
+
+
+        // my vars
+        const byte IDLE = 0, LEFT = 1, UP = 2, RIGHT = 3, DOWN = 4;
+
 
         public Sprite() {
 
@@ -66,7 +80,7 @@ namespace SpriteClasses{
             TextureImage = textureImage;
             Position = position;
             OriginalVelocity = velocity;
-            CurrentVelocity = velocity;
+            Velocity = velocity;
             RotationSpeed = rotationSpeed;
             Scale = scale;
             SpriteEffect = spriteEffect;
@@ -75,15 +89,16 @@ namespace SpriteClasses{
                 SpriteOrigin = new Vector2(TextureImage.Width / 2, TextureImage.Height / 2);
             }
             else {
-                SpriteOrigin = new Vector2(0, 0);
+                SpriteOrigin = Vector2.Zero;
             }
 
-            Active = true;
+            // Renamed 'active' to 'alive' as it made more sense
+            Alive = true;
 
         }
 
         public virtual void Update(GameTime gameTime) {
-            if (Active) {
+            if (Alive) {
                 
                 if (Rotation < 360) {
                     Rotation += RotationSpeed;
@@ -92,19 +107,60 @@ namespace SpriteClasses{
                     Rotation = 0;
                 }
 
-                position.X += (currentVelocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds);
-                position.Y += (currentVelocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                position.X += (velocity.X * (float)gameTime.ElapsedGameTime.TotalSeconds);
+                position.Y += (velocity.Y * (float)gameTime.ElapsedGameTime.TotalSeconds);
             }
         }
 
+        public bool CollisionMouse(int x, int y) {
+            if (CollisionRectangle.Contains(x, y) && Alive){
+                Kill();
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public bool CollisionSprite(Sprite sprite) {
+            return true;
+        }
+
+        public void Kill() {
+            Alive = false;
+            velocity = Vector2.Zero;
+            position = new Vector2(-100, -100);
+
+        }
+
+        public void Controls(byte direction) {
+            if (direction == IDLE) {
+                velocity.X = 0;
+                velocity.Y = 0;
+            } else {
+                if (direction == RIGHT) {
+                    velocity.X += originalVelocity.X;
+                } else if (direction == LEFT) {
+                    velocity.X -= originalVelocity.X;
+                }
+
+                if (direction == UP) {
+                    velocity.Y -= originalVelocity.Y;
+                } else if (direction == DOWN) {
+                    velocity.Y += originalVelocity.Y;
+                }
+            }
+        }
+
+
+
         public virtual void Update(GameTime gameTime, GraphicsDevice Device) {
-            if (Active) {
+            if (Alive) {
                 if ((position.X + SpriteOrigin.X * Scale) >= Device.Viewport.Width || position.X - SpriteOrigin.X * Scale <= 0) {
-                    currentVelocity.X *= -1;
+                    velocity.X *= -1;
                 }
 
                 if ((position.Y + SpriteOrigin.Y * Scale) >= Device.Viewport.Height || position.Y - SpriteOrigin.Y * Scale <= 0) {
-                    currentVelocity.Y *= -1;
+                    velocity.Y *= -1;
                 }
 
                 Update(gameTime);
@@ -112,7 +168,7 @@ namespace SpriteClasses{
         }
 
         public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
-            if (Active) {
+            if (Alive) {
                 spriteBatch.Draw(TextureImage, position, new Rectangle(0, 0, TextureImage.Width, TextureImage.Height), Color.White, MathHelper.ToRadians(Rotation), SpriteOrigin, Scale, SpriteEffect, 0);
             }
         }
